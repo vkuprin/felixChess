8e3d7225c07ac7646596a2ac5a624210da97ca6b
Vitaly Kuprin <vitas_s@inbox.lv>
2023-04-25 13:45:28 +0200

    initial

---
+++ main.ts
@@ -0,0 +1,38 @@
+interface Player {
+    name: string;
+    age: number;
+    score: number;
+}
+
+const data: Player[] = [
+    {
+        name: 'Magnus Carlsen',
+        age: 40,
+        score: 20
+    },
+    {
+        name: 'Fabiano Caruana',
+        age: 30,
+        score: 10
+    },
+    {
+        name: 'Hikaru Nakamura',
+        age: 22,
+        score: 15
+    },
+    {
+        name: 'Ding Liren',
+        age: 33,
+        score: 5
+    },
+    {
+        name: 'Maxime Vachier-Lagrave',
+        age: 30,
+        score: 10
+    },
+    {
+        name: 'AliReza Firouzja',
+        age: 19,
+        score: 10
+    }
+]
