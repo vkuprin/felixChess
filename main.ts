758580524a370cf0a89527e00b1c7e1221f1e343
Vitaly Kuprin <vitas_s@inbox.lv>
2023-04-25 14:22:11 +0200

    fix
    perf: improve speed
    
    chore: increase readability
    
    chore: improve

--- main.ts
+++ main.ts
@@ -4,59 +4,31 @@ interface Player {
     score: number; // elo rating
 }
 
-const data: Player[] = [
-    {
-        name: 'Magnus Carlsen',
-        age: 40,
-        score: 2850,
-    },
-    {
-        name: 'Fabiano Caruana',
-        age: 30,
-        score: 2440,
-    },
-    {
-        name: 'Hikaru Nakamura',
-        age: 22,
-        score: 2760,
-    },
-    {
-        name: 'Ding Liren',
-        age: 33,
-        score: 2820,
-    },
-    {
-        name: 'Maxime Vachier-Lagrave',
-        age: 30,
-        score: 2647,
-    },
-    {
-        name: 'AliReza Firouzja',
-        age: 19,
-        score: 4324,
-    },
+const playersData: Player[] = [
+    { name: 'Magnus Carlsen', age: 40, score: 2850 },
+    { name: 'Fabiano Caruana', age: 30, score: 2440 },
+    { name: 'Hikaru Nakamura', age: 22, score: 2760 },
+    { name: 'Ding Liren', age: 33, score: 2820 },
+    { name: 'Maxime Vachier-Lagrave', age: 30, score: 2647 },
+    { name: 'AliReza Firouzja', age: 19, score: 4324 },
 ];
 
-// Time complexity: ~O(n^2)
-const championsList = (players: Player[]): Player[] => {
+const sortPlayersByScoreAndAge = (a: Player, b: Player): number => {
+    if (b.score !== a.score) return b.score - a.score;
+    return a.age - b.age;
+}
+
+const isPlayerBetter = (player: Player, lastChampion: Player): boolean => {
+    return (player.score > lastChampion.score) && (player.age < lastChampion.age);
+}
+
+// Time complexity: ~O(n*log(n))
+const getChampionsList = (players: Player[]): Player[] => {
     const champions: Player[] = [];
+    const sortedPlayers = players.slice().sort(sortPlayersByScoreAndAge);
 
-    const sortedPlayers = players.slice().sort((a, b) => b.score - a.score);
-
-    sortedPlayers.forEach((player, index) => {
-        if (index === 0) {
-            champions.push(player);
-            return;
-        }
-
-        const isChampion = champions.every((champion) => {
-            const isStrongerOrSameScore = player.score >= champion.score;
-            const isYoungerOrSameAge = player.age <= champion.age;
-
-            return !(isStrongerOrSameScore && isYoungerOrSameAge);
-        });
-
-        if (isChampion) {
+    sortedPlayers.forEach((player) => {
+        if (champions.length === 0 || isPlayerBetter(player, champions[champions.length - 1])) {
             champions.push(player);
         }
     });
@@ -64,4 +36,4 @@ const championsList = (players: Player[]): Player[] => {
     return champions;
 };
 
-console.log(championsList(data));
+console.log(getChampionsList(playersData));
